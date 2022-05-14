8b5f6e45282cdc980bb149f2d81d7b525ef6cc5e
MrBoboGet <emanuelberggren01@gmail.com>
2022-05-14 02:01:18 +0200

    La till filesystem i MBUtility. Lite osäker på huruvida jag vill flytta allt till ett MBSystem namespace, samtidigt är det här förmodligen generellt nog att det kan vara här

---
+++ Filesystem.cpp
@@ -0,0 +1,169 @@
+#include "Filesystem.h"
+#include <map>
+#include <filesystem>
+#include <codecvt>
+namespace MBUtility
+{
+	std::string i_PathToUTF8(std::filesystem::path const& PathToConvert)
+	{
+		std::string ReturnValue;
+#ifdef MBWindows
+		std::u16string source = std::u16string((char16_t*)PathToConvert.c_str());
+		std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+		ReturnValue = convert.to_bytes(source);
+#else
+		return(PathToConvert.generic_u8string());
+#endif
+		return(ReturnValue);
+	}
+
+
+	const char* i_FSErrorTypeToStringMap[] =
+	{
+		"Ok",
+		"Entry doesn't exist",
+		"Invalid Filesystem type",
+		"Invalid Filesystem type",
+		"Unknown"
+	};
+	std::string FSErrorToString(FilesystemError ErrorToConvert)
+	{
+		if (uint32_t(ErrorToConvert) >= sizeof(i_FSErrorTypeToStringMap) / sizeof(const char*))
+		{
+			throw std::runtime_error("Invalid FilesystemType");
+		}
+		return(i_FSErrorTypeToStringMap[size_t(ErrorToConvert)]);
+	}
+
+	FileSystemType i_STDFSTypeToMBFSType(std::filesystem::file_type TypeToConvert)
+	{
+		FileSystemType ReturnValue = FileSystemType::Null;
+		if (TypeToConvert == std::filesystem::file_type::directory)
+		{
+			ReturnValue = FileSystemType::Directory;
+		}
+		else if (TypeToConvert == std::filesystem::file_type::regular)
+		{
+			ReturnValue = FileSystemType::File;
+		}
+		else
+		{
+			//mainly debugging
+			throw std::runtime_error("Invalid filesystem type");
+		}
+		return(ReturnValue);
+	}
+	OS_Filesystem::OS_Filesystem(std::string const& User, std::filesystem::path StartDirectory)
+	{
+		m_CurrentDirectory = std::move(StartDirectory);
+	}
+	std::string OS_Filesystem::GetCurrentPath()
+	{
+		std::string ReturnValue = i_PathToUTF8(std::filesystem::canonical(m_CurrentDirectory));
+		return(ReturnValue);
+	}
+	FilesystemError OS_Filesystem::ChangeDirectory(std::string const& NewDirectory)
+	{
+		FilesystemError ReturnValue = FilesystemError::Ok;
+		std::error_code Error;
+		std::filesystem::path NewPath = m_CurrentDirectory / NewDirectory;
+		if (!std::filesystem::exists(NewPath) || !std::filesystem::is_directory(NewPath))
+		{
+			return FilesystemError::EntryDoesntExist;
+		}
+		m_CurrentDirectory = NewPath;
+		return(ReturnValue);
+	}
+	FSObjectInfo OS_Filesystem::GetInfo(std::string const& Path, FilesystemError* OutError)
+	{
+		FSObjectInfo ReturnValue;
+		std::error_code Error;
+		std::filesystem::directory_entry Entry = std::filesystem::directory_entry(m_CurrentDirectory / Path, Error);
+		if (Error)
+		{
+			*OutError = FilesystemError::Unknown;
+			return(ReturnValue);
+		}
+		ReturnValue.Name = i_PathToUTF8(Entry.path().filename());
+		ReturnValue.Size = Entry.file_size();
+		ReturnValue.Type = i_STDFSTypeToMBFSType(Entry.status().type());
+		return(ReturnValue);
+	}
+	std::vector<FSObjectInfo> OS_Filesystem::ListDirectory(std::string const& Path, FilesystemError* OutError)
+	{
+		std::vector<FSObjectInfo> ReturnValue;
+		std::filesystem::path PathToList = m_CurrentDirectory / Path;
+		if (!std::filesystem::exists(PathToList))
+		{
+			*OutError = FilesystemError::EntryDoesntExist;
+			return(ReturnValue);
+		}
+		if (!std::filesystem::directory_entry(PathToList).is_directory())
+		{
+			*OutError = FilesystemError::InvalidFilesystemType;
+			return(ReturnValue);
+		}
+		std::error_code OSResult;
+		std::filesystem::directory_iterator Iterator(PathToList, OSResult);
+		if (OSResult)
+		{
+			*OutError = FilesystemError::Unknown;
+			return(ReturnValue);
+		}
+		for (auto& Entry : Iterator)
+		{
+			FSObjectInfo NewInfo;
+			NewInfo.Name = i_PathToUTF8(Entry.path().filename());
+			NewInfo.Size = Entry.file_size();
+			NewInfo.Type = i_STDFSTypeToMBFSType(Entry.status().type());
+			ReturnValue.push_back(std::move(NewInfo));
+		}
+		return(ReturnValue);
+	}
+	std::unique_ptr<MBUtility::MBSearchableInputStream> OS_Filesystem::GetFileInputStream(std::string const& FilePath, FilesystemError* OutError)
+	{
+		std::unique_ptr<MBUtility::MBSearchableInputStream> ReturnValue;
+		std::filesystem::path AbsolutePath = m_CurrentDirectory / FilePath;
+		std::error_code Error;
+		if (!std::filesystem::exists(AbsolutePath, Error) || !std::filesystem::is_regular_file(AbsolutePath))
+		{
+			*OutError = FilesystemError::EntryDoesntExist;
+			return(ReturnValue);
+		}
+		//TODO check for acces rights
+		//TODO update MBFileInput stream to include std::filesystem path
+		return(std::unique_ptr<MBUtility::MBFileInputStream>(new MBUtility::MBFileInputStream(AbsolutePath.c_str())));
+	}
+	std::unique_ptr<MBUtility::MBSearchableOutputStream> OS_Filesystem::GetFileOutputStream(std::string const& FilePath, FilesystemError* OutError)
+	{
+		std::unique_ptr<MBUtility::MBOctetOutputStream> ReturnValue;
+		std::filesystem::path AbsolutePath = m_CurrentDirectory / FilePath;
+		//TODO check for acces rights
+		//TODO update MBFileInput stream to include std::filesystem path
+		return(std::unique_ptr<MBUtility::MBFileOutputStream>(new MBUtility::MBFileOutputStream(AbsolutePath.c_str())));
+	}
+	const char* i_InternalFSToStringMap[] = { "Null","Directory","File" };
+	std::string FSTypeToString(FileSystemType TypeToConvert)
+	{
+		std::string ReturnValue;
+		uint64_t TypeValue = uint64_t(TypeToConvert);
+		if (TypeValue > sizeof(i_InternalFSToStringMap) / sizeof(const char*))
+		{
+			throw std::runtime_error("Invalid FSType specified");
+		}
+		ReturnValue = i_InternalFSToStringMap[TypeValue];
+		return(ReturnValue);
+	}
+	//ANTAGANDE thread safe
+	const std::map<std::string, FileSystemType> i_InternalStringToFSMap = { {"Null",FileSystemType::Null},{"Directory",FileSystemType::Directory},{"File",FileSystemType::File} };
+	FileSystemType StringToFSObjectType(std::string const& TypeToConvert)
+	{
+		FileSystemType ReturnValue;
+		if (i_InternalStringToFSMap.find(TypeToConvert) == i_InternalStringToFSMap.end())
+		{
+			throw std::runtime_error("Invalid FilesystemTypeString provided");
+		}
+		ReturnValue = i_InternalStringToFSMap.at(TypeToConvert);
+		return(ReturnValue);
+	}
+};
---
+++ Filesystem.h
@@ -0,0 +1,60 @@
+#include "MBInterfaces.h"
+#include "MBErrorHandling.h"
+
+#include <Filesystem>
+namespace MBUtility
+{
+	enum class FileSystemType
+	{
+		Null,
+		Directory,
+		File,
+	};
+	std::string FSTypeToString(FileSystemType TypeToConvert);
+	FileSystemType StringToFSObjectType(std::string const& TypeToConvert);
+	struct FSObjectInfo
+	{
+		std::string Name;
+		FileSystemType Type = FileSystemType::Null;
+		uint64_t Size = 0;
+	};
+	enum class FilesystemError
+	{
+		Ok,
+		EntryDoesntExist,
+		InvalidFilesystemType,
+		InvalidCredentials,
+		Unknown,
+	};
+	inline bool operator!(FilesystemError Error)
+	{
+		return(Error != FilesystemError::Ok);
+	}
+	std::string FSErrorToString(FilesystemError ErrorToConvert);
+	//ABSTRACT CLASSES
+	class Filesystem
+	{
+	public:
+		virtual std::string GetCurrentPath() = 0;
+		virtual FilesystemError ChangeDirectory(std::string const& NewDirectory) = 0;
+		virtual FSObjectInfo GetInfo(std::string const& Path, FilesystemError* OutError) = 0;
+		virtual std::vector<FSObjectInfo> ListDirectory(std::string const& Path, FilesystemError* OutError) = 0;
+		virtual std::unique_ptr<MBUtility::MBSearchableInputStream> GetFileInputStream(std::string const& FilePath, FilesystemError* OutError) = 0;
+		virtual std::unique_ptr<MBUtility::MBSearchableOutputStream> GetFileOutputStream(std::string const& FilePath, FilesystemError* OutError) = 0;
+	};
+
+	class OS_Filesystem : public Filesystem
+	{
+	private:
+		std::filesystem::path m_CurrentDirectory;
+	public:
+		OS_Filesystem(std::string const& User, std::filesystem::path StartDirectory);
+		//Error Open(std::string const& User, std::filesystem::path StartDirectory);?
+		virtual std::string GetCurrentPath();
+		virtual FilesystemError ChangeDirectory(std::string const& NewDirectory) override;
+		virtual FSObjectInfo GetInfo(std::string const& Path, FilesystemError* OutError) override;
+		virtual std::vector<FSObjectInfo> ListDirectory(std::string const& Path, FilesystemError* OutError) override;
+		virtual std::unique_ptr<MBUtility::MBSearchableInputStream> GetFileInputStream(std::string const& FilePath, FilesystemError* OutError) override;
+		virtual std::unique_ptr<MBUtility::MBSearchableOutputStream> GetFileOutputStream(std::string const& FilePath, FilesystemError* OutError) override;
+	};
+}
